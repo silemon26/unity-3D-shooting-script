4de491b9fd5e6fc27eeb9d185f0b671a4e9d586d
silemon26 <155801631+silemon26@users.noreply.github.com>
2024-01-20 18:37:27 +0900

    Update PlayerController.cs

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,48 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rigid;
 
-    public int JumpForce;
-    public int MoveSpeed;
+    public Transform cameraTransform;
+
+    public CharacterController controller;
+    public float JumpForce;
+    public float MoveSpeed;
+    public float Yvelocity = 0;
+    public float Gravity = -9.8f;
+    
+   
 
     private bool IsJumping;
     private void Start()
     {
-        rigid = GetComponent<Rigidbody>();    
+        rigid = GetComponent<Rigidbody>();   
+        controller=GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        Move();
-        Jump();
-    }
-
-    void Move()
-    {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        transform.Translate((new Vector3(h, 0, v) * MoveSpeed) * Time.deltaTime);
+        Vector3 movedirection = new Vector3(h, 0, v);
+
+        movedirection = cameraTransform.TransformDirection(movedirection);
+        // 카메라 위치
+        movedirection *= MoveSpeed;
+
+        if (controller.isGrounded)
+        {
+            Yvelocity = 0;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Yvelocity = JumpForce;
+            }
+        }
+        Yvelocity += Gravity * Time.deltaTime;
+
+        movedirection.y = Yvelocity;
+
+        controller.Move(movedirection * Time.deltaTime);
     }
 
-    void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (!IsJumping)
-            {
-                IsJumping = true;
-                rigid.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
-            }
-        } else {
-            return;
-        }
-            
-    }
+    
+
+ 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
